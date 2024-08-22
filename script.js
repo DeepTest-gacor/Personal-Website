7b60cff31ce8f271924476626a5566904ba8c2ff
DeepTest-gacor <agungplg104@gmail.com>
2024-08-23 06:16:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('action-button').addEventListener('click', function() {
+    document.body.style.background = 'linear-gradient(135deg, #00b4d8, #90e0ef)';
+    this.textContent = 'Wow!!';
+});
